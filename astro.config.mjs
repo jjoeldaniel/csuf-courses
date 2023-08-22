783d5017d2b640964a7337a25f180d895d0c8890
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-08-21 21:00:09 -0700

    revert hybrid

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,9 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
-import nodejs from "@astrojs/node";
-
-import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
-  adapter: node({
-    mode: "standalone",
-  }),
-  output: "hybrid",
   redirects: {
     "/course": "/",
   },
