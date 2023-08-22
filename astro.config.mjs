4eec90ddd7d3c86b296f1c8da6cbac6a4d5c6769
jjoeldaniel <joeldanielrico@csu.fullerton.edu>
2023-08-21 20:45:19 -0700

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,17 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
+import nodejs from "@astrojs/node";
+
+import node from "@astrojs/node";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind()],
+  adapter: node({
+    mode: "standalone",
+  }),
+  output: "hybrid",
+  redirects: {
+    "/course": "/",
+  },
 });
